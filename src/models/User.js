33a49964e20003901409f1f56021fddc7d43e98a
Mike Farrow <contact@mikefarrow.co.uk>
2016-02-12 21:41:58 +0000

    prevent users from revoking their own keystone access

--- src/models/User.js
+++ src/models/User.js
@@ -10,7 +10,6 @@ User.add({
   password: { type: Types.Password, initial: true, required: false },
   profileImage: { type: Types.CloudinaryImage, autoCleanup: true }
 }, 'Permissions', {
-  // canAccessKeystoneOld: { type: Boolean, noedit: true, hidden: true },
   canAccessKeystone: { type: Boolean, initial: true, default: false },
 });
 
@@ -18,31 +17,13 @@ User.schema.methods.wasActive = function () {
   this.lastActiveOn = new Date();
   return this;
 };
-//
-// User.schema.path('canAccessKeystoneOld').get(function(value) {
-//   if(value === undefined)
-//     return this.get('canAccessKeystone');
-//
-//   return value;
-// });
-//
-// User.schema.path('canAccessKeystoneOld').set(function(value) {
-//   return value;
-// });
-//
-// User.schema.path('canAccessKeystone').set(function(value) {
-//   this.set('canAccessKeystoneOld', this.get('canAccessKeystone'));
-//   return value;
-// });
-//
-// User.schema.pre('save', function(next, done) {
-//   if(this._id && this._req_user && this._id.equals(this._req_user._id)){
-//     var canAccessKeystoneOld = this.get('canAccessKeystoneOld');
-//     this.set('canAccessKeystone', canAccessKeystoneOld);
-//     this.set('canAccessKeystoneOld', canAccessKeystoneOld);
-//   }
-//   next();
-// });
+
+User.schema.pre('save', function(next, done) {
+  if(this._id && this._req_user && this._id.equals(this._req_user._id)){
+    this.set('canAccessKeystone', true);
+  }
+  next();
+});
 
 User.track = true;
 User.defaultColumns = 'name, email';
