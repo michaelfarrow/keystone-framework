d84518f898ec03ca1b170c3b2e73af054cfdca03
Mike Farrow <contact@mikefarrow.co.uk>
2016-04-07 00:39:50 +0100

    adding dev seeder

---
+++ src/updates/1.0.0-dev-seed.js
@@ -0,0 +1,15 @@
+
+// var keystone = require('keystone'),
+//     User = keystone.list('User');
+
+exports = module.exports = function(done) {
+
+  done();
+
+  // if(keystone.get('env') == 'production') {
+  //   done();
+  // }else{
+  //   // Do some seeding
+  // }
+
+};
