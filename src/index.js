0208472381ab69394b701f0e714c5c9660714a7d
Mike Farrow <contact@mikefarrow.co.uk>
2016-04-09 00:08:08 +0100

    ensuring database compatability with Dokku

--- src/index.js
+++ src/index.js
@@ -33,7 +33,8 @@ keystone.init({
   'views': 'templates/views',
   'view engine': 'jade',
 
-  'mongo': 'mongodb://'
+  'mongo': process.env.MONGO_URL ||
+    'mongodb://'
     + process.env.MONGO_PORT_27017_TCP_ADDR
     + ':' + process.env.MONGO_PORT_27017_TCP_PORT
     + '/project',
