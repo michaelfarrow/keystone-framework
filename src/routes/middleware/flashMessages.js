bcbcba11e387f42ea8d667992539cbf46f2ece8d
Mike Farrow <contact@mikefarrow.co.uk>
2016-05-27 13:00:54 +0100

    adding old input

--- src/routes/middleware/flashMessages.js
+++ src/routes/middleware/flashMessages.js
@@ -13,9 +13,16 @@ exports = module.exports = function(req, res, next) {
     error: req.flash('error')
   };
 
+  // get old req.body and then flash the current req.body for next request
+  var oldInput = req.flash('oldInput');
+  req.flash('oldInput', req.body);
+
+
   res.locals.messages = _.some(flashMessages, function(msgs) {
     return msgs.length;
   }) ? flashMessages : false;
+
+  res.locals.oldInput = oldInput && oldInput.length ? oldInput[0] : {};
 
   next();
 
