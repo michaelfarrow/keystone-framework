4f536c039b701e1bde69e84b2e3d8728e4cfaec2
Mike Farrow <contact@mikefarrow.co.uk>
2016-06-05 07:32:17 +0100

    upgrading keystone-pages, closes #13

--- src/config/templates.js
+++ src/config/templates.js
@@ -1,16 +1,48 @@
 
-var keystone = require('keystone');
+// var keystone = require('keystone');
 // var Types = keystone.Field.Types;
 
-keystone.set('templates', {
-  // 'Default': {
-  //   text: { type: Types.Text },
-  //   textarea: { type: Types.Textarea },
-  // },
-  // 'Home': {
-  //   text: { type: Types.Text },
-  // },
-});
+// keystone.set('template options', {
+//   track: false,
+// });
+
+// keystone.set('templates global', {
+//   displayTitle: { type: Types.Text },
+// });
+
+// keystone.set('templates global footer', [
+//   { heading: 'Linkable' },
+//   { linkable: {
+//       title: { type: Types.Text },
+//       buttonTitle: { type: Types.Text, default: 'See More' },
+//       description: { type: Types.Html, wysiwyg: true },
+//     },
+//   },
+// ]);
+
+// keystone.set('templates', {
+//   'default': {
+//     text: { type: Types.Text },
+//     textarea: { type: Types.Textarea },
+//   },
+//   'home': {
+//     text: { type: Types.Text },
+//   },
+// });
+
+// keystone.set('template virtuals', {
+//   'home.isActive': function(){
+//     return this.fields.live;
+//   },
+// });
+
+// keystone.set('template methods', {
+//   'home.allPages': function(callback){
+//     Page.model.find()
+//       .where('template').ne('home')
+//       .exec(callback);
+//   },
+// });
 
 // keystone.set('templates validation', {
 //   'Default': function(callback){
