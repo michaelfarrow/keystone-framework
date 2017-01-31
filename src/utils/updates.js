8476708731fc3eb60072e35b585ce432e49f2de4
Mike Farrow <contact@mikefarrow.co.uk>
2017-01-31 16:46:42 +0000

    simplifying utils/updates

--- src/utils/updates.js
+++ src/utils/updates.js
@@ -6,16 +6,12 @@ updates.saveSingleModel = function(model, callback){
   model.save(callback);
 };
 
-updates.saveSingleModels = function(models){
-  return function(callback){
-    async.eachSeries(models, updates.saveSingleModel, callback);
-  };
+updates.saveSingleModels = function(models, callback){
+  async.eachSeries(models, updates.saveSingleModel, callback);
 };
 
-updates.saveModels = function(list, data){
-  return function(callback){
-    list.model.create(data, callback);
-  };
+updates.saveModels = function(list, data, callback){
+  list.model.create(data, callback);
 };
 
 exports = module.exports = updates;
