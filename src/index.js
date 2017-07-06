d35cf4ed9a7d54a5304d56d8997ba1502f01c75d
Mike Farrow <contact@mikefarrow.co.uk>
2017-07-06 15:19:18 +0100

    updating lib and config paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('app-module-path').addPath(__dirname);
 
-require('./lib/console')
+require('lib/console')
 
 console.log('Starting')
 var startTime = new Date().getTime()
@@ -8,34 +8,34 @@ var keystone = require('keystone')
 
 keystone.init()
 
-require('./config/env')
+require('config/env')
 
-require('./config/app')
-require('./config/log')
-require('./config/db')
-require('./config/cookies')
-require('./config/sessions')
-require('./config/auth')
-require('./config/views')
-require('./config/assets')
-require('./config/images')
-require('./config/options')
-require('./config/templates')
-require('./config/wysiwyg')
+require('config/app')
+require('config/log')
+require('config/db')
+require('config/cookies')
+require('config/sessions')
+require('config/auth')
+require('config/views')
+require('config/assets')
+require('config/images')
+require('config/options')
+require('config/templates')
+require('config/wysiwyg')
 
-require('./config/models')
-require('./config/routes')
+require('config/models')
+require('config/routes')
 
 // Load and initialise plugins.
 require('keystone-options').init(keystone)
 require('keystone-pages').init(keystone)
 
 // Load nav after all our models have been loaded.
-require('./config/nav')
+require('config/nav')
 
 // Finally, start Keystone
 keystone.start(function () {
   var endTime = new Date().getTime()
   console.log('Loaded in', ((endTime - startTime) / 1000) + 's')
-  require('./utils/watch')
+  require('utils/watch')
 })
