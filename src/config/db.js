5f20f1daa26610075ea16323b95f3ed5151e6c52
Mike Farrow <contact@mikefarrow.co.uk>
2018-03-29 21:29:09 +0100

    adding werckit and removing some redundant scripts/code

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,3 @@
 var keystone = require('keystone')
 
-keystone.set('mongo', process.env.MONGO_URL ||
-  'mongodb://' +
-  process.env.MONGO_PORT_27017_TCP_ADDR +
-  ':' + process.env.MONGO_PORT_27017_TCP_PORT +
-  '/project'
-)
+keystone.set('mongo', process.env.MONGO_URL || 'mongodb://db:27017/project')
